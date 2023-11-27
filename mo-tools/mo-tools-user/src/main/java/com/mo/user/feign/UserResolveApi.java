ffbdff5dd81f5ac8e6023fd8509a8d73d18f120f
moweiquan <295597613@qq.com>
2023-11-28 00:17:46 +0800

    tools模块-集成user项目

---
+++ mo-tools/mo-tools-user/src/main/java/com/mo/user/feign/UserResolveApi.java
@@ -0,0 +1,21 @@
+package com.mo.user.feign;
+
+import com.mo.base.R;
+import com.mo.user.feign.fallback.UserResolveApiFallback;
+import com.mo.user.model.SysUser;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+/**
+ * 用户操作API
+ */
+@FeignClient(name = "${mo.feign.authority-server:mo-auth-server}", fallbackFactory = UserResolveApiFallback.class)
+public interface UserResolveApi {
+    /**
+     * 根据id 查询用户详情
+     */
+    @PostMapping(value = "/user/anno/id/{id}")
+    R<SysUser> getById(@PathVariable("id") Long id, @RequestBody UserQuery userQuery);
+}
