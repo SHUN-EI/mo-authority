df68f797a2f40eb7e7b837c761d34c3009fd8767
moweiquan <295597613@qq.com>
2023-12-02 00:20:46 +0800

    权限系统-权限模块-公共基础配置类

---
+++ mo-apps/mo-auth/mo-auth-server/src/main/java/com/mo/authority/config/AuthorityWebConfiguration.java
@@ -0,0 +1,12 @@
+package com.mo.authority.config;
+
+import com.mo.common.config.BaseConfig;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by mo on 2023/12/2
+ * 公共基础配置类
+ */
+@Configuration
+public class AuthorityWebConfiguration  extends BaseConfig {
+}
